c12e17fba5143222d8217af9d2d40ca34397a0cf
balasubramaniyand <132192864+balasubramaniyand@users.noreply.github.com>
2024-01-31 16:03:40 +0530

    Create details about this node.js

---
+++ details about this node.js
@@ -0,0 +1,12 @@
+first index.js 
+app.js 
+packeage.js
+this file after create images
+this will be comes may be 
+showing error need index.js file
+
+docker the docker container go the outside people get the result
+
+
+
+
